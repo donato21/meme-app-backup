7c76aafd0e240e69f3b91ec2554233166e9dc4f1
Donato Isai Clemente <47828637+donatoisai16@users.noreply.github.com>
2020-11-22 02:56:41 -0600

    functional actionBar customization

--- app/src/main/java/com/cs3326/projectmeme/AppActivity.java
+++ app/src/main/java/com/cs3326/projectmeme/AppActivity.java
@@ -3,12 +3,27 @@ package com.cs3326.projectmeme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AppActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Explore");
         setContentView(R.layout.activity_app);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void changeToProfileFragment(MenuItem mi) {
+        //TODO: Change fragment code here
+        System.out.println("Menu Profile Icon Clicked! ");
     }
 }
