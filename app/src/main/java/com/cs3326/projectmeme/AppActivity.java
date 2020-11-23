94f224cf09d81fdd9b3612e74f51fee87d034c68
Donato Isai Clemente <47828637+donatoisai16@users.noreply.github.com>
2020-11-22 22:40:06 -0600

    Changed over firestore init to AppActivity

--- app/src/main/java/com/cs3326/projectmeme/AppActivity.java
+++ app/src/main/java/com/cs3326/projectmeme/AppActivity.java
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.cs3326.projectmeme.adapter.PostAdapter;
 import com.cs3326.projectmeme.app.profile.ProfileFragment;
 import com.cs3326.projectmeme.app.timeline.TimelineFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.Query;
 
 public class AppActivity extends AppCompatActivity {
+
+    // TODO: Implement adapter
+    private RecyclerView mPostsRecycler;
+    private Query mQuery;
+    private PostAdapter mAdapter;
+    private ViewGroup mEmptyView;
+    // Firebase Auth
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    TextView emailTextView, displayNameTextView;
     Menu menu;
 
     @Override
@@ -23,6 +41,8 @@ public class AppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
         setTitle("");
+
+        getSupportFragmentManager().getFragments();
     }
 
     @Override
@@ -30,7 +50,6 @@ public class AppActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         this.menu = menu;
         getMenuInflater().inflate(R.menu.actionbar, menu);
-
         // On init change to timeline frag
         changeToTimelineFragment();
         return true;
@@ -44,6 +63,12 @@ public class AppActivity extends AppCompatActivity {
     private void changeToProfileFragment() {
         menu.findItem(R.id.miProfile).setVisible(false);
         changeToFragment(new ProfileFragment());
+    }
+
+    public void onSignOutClick(View view){
+        mAuth.signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     private void changeToFragment(Fragment fragment){
@@ -60,5 +85,23 @@ public class AppActivity extends AppCompatActivity {
 
     public void onToTimelineClicked(View view) {
         changeToTimelineFragment();
+    }
+
+    // TODO: initRecyclerView
+    private void initRecylclerView() {
+        if( mQuery == null ) {
+            //Error
+        }
+
+        mAdapter = new PostAdapter(mQuery, (PostAdapter.OnPostSelecterListener) this) {
+            @Override
+            protected void onDataChanged() {
+
+            }
+        };
+
+        // TODO: set layout manager & set adapter
+        mPostsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mPostsRecycler.setAdapter(mAdapter);
     }
 }
