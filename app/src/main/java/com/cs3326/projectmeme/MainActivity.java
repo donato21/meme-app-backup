1aa25458582067e9ecd683f1175814a96acd1c24
benn511 <bgomez190@gmail.com>
2020-11-22 18:36:01 -0600

    Setup for RecyclerView and adapter

--- app/src/main/java/com/cs3326/projectmeme/MainActivity.java
+++ app/src/main/java/com/cs3326/projectmeme/MainActivity.java
@@ -4,10 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cs3326.projectmeme.adapter.PostAdapter;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO: Implement adapter
+    private RecyclerView mPostsRecycler;
+    private Query mQuery;
+    private PostAdapter mAdapter;
+    private ViewGroup mEmptyView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +49,23 @@ public class MainActivity extends AppCompatActivity {
     public void changeToSplashActivity() {
         Intent i = new Intent(this, SplashActivity.class);
         startActivity(i);
+    }
+
+    // TODO: initRecyclerView
+    private void initRecylclerView() {
+        if( mQuery == null ) {
+            //Error
+        }
+
+        mAdapter = new PostAdapter(mQuery, (PostAdapter.OnPostSelecterListener) this) {
+            @Override
+            protected void onDataChanged() {
+
+            }
+        };
+
+        // TODO: set layout manager & set adapter
+        mPostsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mPostsRecycler.setAdapter(mAdapter);
     }
 }
